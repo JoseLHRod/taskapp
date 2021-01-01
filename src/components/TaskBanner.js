b8e434f6914f9840898b76ae2dd78d7c8ac6a47d
Jose Luis Hernandez <hernandezrjosel@gmail.com>
2021-01-01 18:06:37 -0400

    first commit

--- src/components/TaskBanner.js
+++ src/components/TaskBanner.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 export const TaskBanner = props => (
     <h4 className="bg-primary text-white text-center p-4">
-        {props.userName}'s Task App ({props.taskItems.filter(t => !t.done).length} Tareas por Hacer)
+        {props.userName}'s Task App ({props.taskItems.filter(t => !t.done).length} Tareas por Realizar)
     </h4>
 )
